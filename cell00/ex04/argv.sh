e7988662f7bc30f387c5bb3469d9676fc01cb605
GBOUT01 <145974794+GBOUT01@users.noreply.github.com>
2024-10-21 15:51:25 +0700

    ex04-commit2

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-#!/bin/sh
-
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
 else
-    count=0  # Initialize count
+    count=0  
     for arg in "$@"; do
-        echo "$arg"  # Print the current argument
-        count=$((count + 1))  # Increment count
-        if [ "$count" -ge 3 ]; then  # Stop after printing 3 arguments
+        echo "$arg"  
+        count=$((count + 1))
+        if [ "$count" -ge 3 ]; then  
             break
         fi
     done
